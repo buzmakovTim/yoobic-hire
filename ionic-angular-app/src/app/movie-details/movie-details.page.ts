01f8211b1a533f6b4b1e1bda552d63a227afbf97
buzmakovTim <buzmakov1987@gmail.com>
2022-02-06 12:39:53 -0800

    FEAT: movie-details.page.ts
    basic view and features added

--- ionic-angular-app/src/app/movie-details/movie-details.page.ts
+++ ionic-angular-app/src/app/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
+import { MoviesService } from './../services/movies.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  information = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private moviesService: MoviesService) { }
 
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    console.log('ID before request: ', id);
+
+    this.moviesService.getDetails(id).subscribe(result => {
+      console.log('ID: ', id);
+      console.log('details', result);
+      this.information = result;
+    });
   }
+
+  openWebSite() {
+    window.open(this.information.Website, '_blank');
+  }
+
+
 
 }
