3329807867802513683083d700f7ee470dc34fa5
buzmakovTim <buzmakov1987@gmail.com>
2022-02-06 07:51:23 -0800

    FEAT: app.component.html
    side menu added with minimal features

--- ionic-angular-app/src/app/app.component.ts
+++ ionic-angular-app/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  public sideMenuLinks = [
+    { title: 'Dashboard', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Photos', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Available Missions', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Master List', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Chat', url: '/folder/Chat', icon: 'trash' },
+  ];
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
   constructor() {}
 }
