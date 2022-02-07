9b52b797217a51efbe648a532a5075048a452b9d
buzmakovTim <buzmakov1987@gmail.com>
2022-02-06 19:48:51 -0800

    FEAT: login-page.page.ts
    login validator added, works as long as email contains '@' and pasword not
    an empty string

--- ionic-angular-app/src/app/login-page/login-page.page.ts
+++ ionic-angular-app/src/app/login-page/login-page.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPagePage implements OnInit {
 
-  constructor() { }
+  emailInputValue = '';
+  passwordInputValue = '';
+
+  constructor(
+    private router: Router,
+    private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  loginValidator(){
+    // email should have @ and both email and password should not be an empty string
+    if(this.passwordInputValue === '' || !this.emailInputValue.includes('@')){
+
+      // Show Invalid Input alert
+      this.alertCtrl.create({
+        header: 'Invalid Input',
+        message: 'Please enter valid email and password',
+        buttons: ['Ok']
+      }).then(alertEl => {
+        alertEl.present();
+      });
+      // Valid input redirect to movies-page
+    } else {
+      this.router.navigateByUrl('/movies-page');
+      this.emailInputValue = '';
+      this.passwordInputValue = '';
+    }
   }
 
 }
